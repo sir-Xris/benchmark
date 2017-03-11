a8d6a4d4d2694bef26618742cc24f6021c1aa6b4
sir-Xris <sir@xris.co>
2017-03-12 04:09:53 +0800

    fix: trap not work

--- mk.cc
+++ mk.cc
@@ -1,12 +1,11 @@
 #include <random>
-#include <cstdio>
+#include <iostream>
 using namespace std;
 
-random_device rd;
-mt19937_64 seed(rd());
+random_device seed;
 uniform_int_distribution<long long> unif(1ll << 63, ~0ull >> 1);
 
 int main() {
-    printf("%lld %lld\n", unif(seed), unif(seed));
+    cout << unif(seed) << " " << unif(seed) << endl;
     return 0;
 }
